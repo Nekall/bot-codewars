b9c469df96ff0aca40d984dab7df0babaa85511d
Neka <nekoanchor@yahoo.fr>
2021-07-13 14:24:49 +0200

    feat: add script start

--- commands/infos.js
+++ commands/infos.js
@@ -1,3 +1,15 @@
+const fetch = require('node-fetch');
+
+client.on('message', async message => {
+	// ...
+  url = link = "https://www.codewars.com/api/v1/users/" + args;
+	if (command === '!cw' && arg !== undefined) {
+		const { file } = await fetch(link).then(response => response.json());
+		message.channel.send(file);
+	}
+});
+
+/*
 client.on('message', message => {
 	if (message.content.startsWith(`${prefix}ping`)) {
 		message.channel.send('Pong.');
@@ -5,3 +17,4 @@ client.on('message', message => {
 		message.channel.send('Boop.');
 	}
 });
+*/
