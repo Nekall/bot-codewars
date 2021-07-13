e4b5a2e81c280441bf11b97f439da137fe625a03
Neka <nekoanchor@yahoo.fr>
2021-07-13 15:39:39 +0200

    feat: add test

--- commands/infos.js
+++ commands/infos.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
+require('dotenv').config()
 
 client.on('message', async message => {
-	// ...
   url = link = "https://www.codewars.com/api/v1/users/" + args;
 	if (command === '!cw' && arg !== undefined) {
 		const { file } = await fetch(link).then(response => response.json());
@@ -9,12 +9,21 @@ client.on('message', async message => {
 	}
 });
 
-/*
+
 client.on('message', message => {
-	if (message.content.startsWith(`${prefix}ping`)) {
+	if (message.content.startsWith(`Ping`)) {
 		message.channel.send('Pong.');
-	} else if (message.content.startsWith(`${prefix}beep`)) {
+	} else if (message.content.startsWith(`Beep`)) {
 		message.channel.send('Boop.');
 	}
 });
-*/
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = process.env.BOT_TOKEN;
+
+client.once('ready', () => {
+   console.log("Félicitations, votre bot Discord a été correctement initialisé !");
+});
+
+client.login(token);
