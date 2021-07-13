cd1e66704ccec7783a9d660c686b9456ef1539a6
Neka <nekoanchor@yahoo.fr>
2021-07-13 13:26:39 +0200

    feat: add fetch node

---
+++ commands/infos.js
@@ -0,0 +1,7 @@
+client.on('message', message => {
+	if (message.content.startsWith(`${prefix}ping`)) {
+		message.channel.send('Pong.');
+	} else if (message.content.startsWith(`${prefix}beep`)) {
+		message.channel.send('Boop.');
+	}
+});
