b4c55cf663fedebd1bb3836516093cc596e036e2
Neka <nekoanchor@yahoo.fr>
2021-07-16 16:11:30 +0200

    feat: more info in lang cmd

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ client.on("message", async message => {
         .setFooter("Dev par Nekå", "https://raw.githubusercontent.com/Nekall/bot-codewars/main/codewars.png");
 
         for (let lang in userData.ranks.languages) {
-          userDataEmbed.addField(lang, "")
+          userDataEmbed.addField(lang, "-")
         }
       message.channel.send(userDataEmbed);
     } else if(command === "cw" && args.length === 0) {
